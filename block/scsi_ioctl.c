85e26118c952895efba792e1bcaac246e6943865
Paolo Bonzini <pbonzini@redhat.com>
2018-01-10 16:54:52 +0100

    UPSTREAM: block: silently forbid sending any ioctl to a partition
    cherry picked from:
     commit 0478fe68685a ("block: silently forbid sending any ioctl to a partition")
    
    After the first few months, the message has not led to many bug reports.
    It's been almost five years now, and in practice the main source of
    it seems to be MTIOCGET that someone is using to detect tape devices.
    While we could whitelist it just like CDROM_GET_CAPABILITY, this patch
    just removes the message altogether.
    
    The patch also removes the "safe but not very useful" ioctl whitelist,
    as suggested by Christoph.  I doubt anything is using most of those
    ioctls _in general_, let alone on a partition.
    
    Bug: 73949641
    Change-Id: Ib95bdfd3a8d4ee56fc4f4af64ea944ded2801768
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>

--- block/scsi_ioctl.c
+++ block/scsi_ioctl.c
@@ -707,49 +707,8 @@ int scsi_verify_blk_ioctl(struct block_device *bd, unsigned int cmd)
 	if (bd && bd == bd->bd_contains)
 		return 0;
 
-	/* Actually none of these is particularly useful on a partition,
-	 * but they are safe.
-	 */
-	switch (cmd) {
-	case SCSI_IOCTL_GET_IDLUN:
-	case SCSI_IOCTL_GET_BUS_NUMBER:
-	case SCSI_IOCTL_GET_PCI:
-	case SCSI_IOCTL_PROBE_HOST:
-	case SG_GET_VERSION_NUM:
-	case SG_SET_TIMEOUT:
-	case SG_GET_TIMEOUT:
-	case SG_GET_RESERVED_SIZE:
-	case SG_SET_RESERVED_SIZE:
-	case SG_EMULATED_HOST:
-		return 0;
-	case CDROM_GET_CAPABILITY:
-		/* Keep this until we remove the printk below.  udev sends it
-		 * and we do not want to spam dmesg about it.   CD-ROMs do
-		 * not have partitions, so we get here only for disks.
-		 */
-		return -ENOIOCTLCMD;
-#ifdef CONFIG_MTK_UFS_SUPPORT
-	/*
-	 * MTK PATCH: bypass CAP_SYS_RAWIO checking for UFS ioctl facility.
-	 */
-	case UFS_IOCTL_FFU:
-		return 0;
-	case UFS_IOCTL_QUERY:
-		return 0;
-	case UFS_IOCTL_GET_FW_VER:
-		return 0;
-#endif
-
-	default:
-		break;
-	}
-
 	if (capable(CAP_SYS_RAWIO))
 		return 0;
-
-	/* In particular, rule out all resets and host-specific ioctls.  */
-	printk_ratelimited(KERN_WARNING
-			   "%s: sending ioctl %x to a partition!\n", current->comm, cmd);
 
 	return -ENOIOCTLCMD;
 }
