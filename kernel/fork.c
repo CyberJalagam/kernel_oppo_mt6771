b745b7e7b0d3f60954aef5a6cb9c2bc1f6412f2e
Sultan Alsawaf <sultan@kerneltoast.com>
2019-06-24 07:22:55 -0700

    simple_lmk: Introduce Simple Low Memory Killer for Android
    Signed-off-by: JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1,4 +1,4 @@
-/*
+"/*
  *  linux/kernel/fork.c
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
@@ -78,6 +78,9 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
+#include <linux/simple_lmk.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -734,6 +737,7 @@ static inline void __mmput(struct mm_struct *mm)
 	}
 	if (mm->binfmt)
 		module_put(mm->binfmt->module);
+	simple_lmk_mm_freed(mm);
 	mmdrop(mm);
 }
 
