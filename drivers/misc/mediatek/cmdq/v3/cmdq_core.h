13dd1aa62b614f1766860e6c88b08420a697eb62
Vaisakh Murali <mvaisakh@statixos.com>
2021-02-22 20:48:57 +0530

    drivers: misc: mtk-cmdq: Purge as much CMDQ_LOGs as possible
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Signed-off-by: Debayan <kardebayan3@gmail.com>
    Signed-off-by: JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>

--- drivers/misc/mediatek/cmdq/v3/cmdq_core.h
+++ drivers/misc/mediatek/cmdq/v3/cmdq_core.h
@@ -87,14 +87,14 @@ struct DumpFirstErrorStruct {
 
 #define CMDQ_LOG(string, args...) \
 do {			\
-	pr_notice("[CMDQ]"string, ##args); \
+	pr_debug("[CMDQ]"string, ##args); \
 	cmdq_core_save_first_dump("[CMDQ]"string, ##args); \
 } while (0)
 
 #define CMDQ_MSG(string, args...) \
 do {			\
 if (cmdq_core_should_print_msg()) { \
-	pr_notice("[CMDQ]"string, ##args); \
+	pr_debug("[CMDQ]"string, ##args); \
 }			\
 } while (0)
 
@@ -108,7 +108,7 @@ if (cmdq_core_should_print_msg()) { \
 
 #define CMDQ_ERR(string, args...) \
 do {			\
-	pr_notice("[CMDQ][ERR]"string, ##args); \
+	pr_err("[CMDQ][ERR]"string, ##args); \
 	cmdq_core_save_first_dump("[CMDQ][ERR]"string, ##args); \
 } while (0)
 
