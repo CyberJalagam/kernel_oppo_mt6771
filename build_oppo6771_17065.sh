ab2a2246638a7f476940fa6d8d11bb315ee75bcb
Gagan Malvi <malvi@aospa.co>
2021-07-17 13:03:22 +0000

    oppo6771: script: Copy modules to AK3
    Signed-off-by: JAISHNAVPRASAD <rbinternationalnetwork@gmail.com>

--- build_oppo6771_17065.sh
+++ build_oppo6771_17065.sh
@@ -94,5 +94,14 @@ compile
 
 if [ $SUCCESS -eq 0 ]
 then
+       mkdir -p AnyKernel/modules/vendor/lib/modules
+	cp -r out/drivers/misc/mediatek/connectivity/bt/mt66xx/legacy/bt_drv.ko AnyKernel/modules/vendor/lib/modules
+        cp -r out/drivers/misc/mediatek/connectivity/common/wmt_drv.ko AnyKernel/modules/vendor/lib/modules
+        cp -r out/drivers/misc/mediatek/connectivity/fmradio/fmradio_drv.ko AnyKernel/modules/vendor/lib/modules
+        cp -r out/drivers/misc/mediatek/connectivity/gps/gps_drv.ko AnyKernel/modules/vendor/lib/modules
+        cp -r out/drivers/misc/mediatek/connectivity/wlan/adaptor/wmt_chrdev_wifi.ko AnyKernel/modules/vendor/lib/modules
+        cp -r out/drivers/misc/mediatek/connectivity/wlan/core/gen3/wlan_drv_gen3.ko AnyKernel/modules/vendor/lib/modules
+        cp -r out/drivers/misc/mediatek/met/met.ko AnyKernel/modules/vendor/lib/modules
+        cp -r out/drivers/misc/mediatek/performance/fpsgo_cus/fpsgo.ko AnyKernel/modules/vendor/lib/modules
 	zipping
 fi
